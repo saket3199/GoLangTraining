6274ad8d8cb5e32527962abad5d45bf7c493cc80
SAket MIshra <saketmishra247@gmail.com>
2021-09-18 18:37:51 +0530

    chp2

--- chp2/guess_game/main/main.go
+++ chp2/guess_game/main/main.go
@@ -1,23 +1,23 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"math/rand"
-	"os"
-	"strconv"
 )
 
 func main() {
 
-	target := rand.Int63n(100) + 1
+	target := rand.Intn(100) + 1
 	fmt.Println(target)
-	reader := bufio.NewReader(os.Stdin)
+	// reader := bufio.NewReader(os.Stdin)
 	for i := 0; i < 10; i++ {
 		fmt.Println("Enter a Guess")
-		input, _ := reader.ReadString('\n')
-		guess, _ := strconv.ParseInt(input, 0, 32)
+		// input, _ := reader.ReadString('\n')
+		// guess, _ := strconv.ParseInt(input, 0, 32)
+		var guess int
+		fmt.Scanln(&guess)
 		fmt.Println(guess)
+
 		if guess == target {
 			fmt.Println("Yay!! You guessed it right !")
 			break
